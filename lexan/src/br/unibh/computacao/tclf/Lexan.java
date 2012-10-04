cada110fc1f4c5dfc523a8e368194d564aa3b6d9
rodrigosor@gmail.com <rodrigosor@gmail.com@581695ff-4f8b-97d0-b0e1-3f380924e67e>
2012-10-04 17:52:02 +0000

    Startup do Projeto.

---
+++ lexan/src/br/unibh/computacao/tclf/Lexan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unibh.computacao.tclf;
+
+/**
+ *
+ * @author FATORM_05
+ */
+public class Lexan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
