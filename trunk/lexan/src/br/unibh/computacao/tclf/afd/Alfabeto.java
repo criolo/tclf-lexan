a97f6e1de4511549cbbf9f1efcc47bc18faf3abd
rodrigosor@gmail.com <rodrigosor@gmail.com@581695ff-4f8b-97d0-b0e1-3f380924e67e>
2012-10-04 17:52:02 +0000

    Startup do Projeto.

---
+++ trunk/lexan/src/br/unibh/computacao/tclf/afd/Alfabeto.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unibh.computacao.tclf.afd;
+
+/**
+ *
+ * @author FATORM_05
+ */
+public class Alfabeto {
+    
+}
